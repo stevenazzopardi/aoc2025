664835b49d292dd2ce7afcc7d6bd1be587c68de0
Steven Azzopardi <stevenazz@gmail.com>
2025-12-02 00:55:50 +0100

    first exercise

---
+++ exercise1-pt1.ex
@@ -0,0 +1,21 @@
+{:ok, contents} = File.read("puzzle1input.txt")
+_startPoint = 50
+filterContents = contents |> String.split("\n", trim: true)
+chunks = [{"C", "50"} | Enum.map(filterContents, fn x -> String.split_at(x, 1) end)]
+abc = Enum.map_reduce(chunks, 0, fn x, acc -> remain = rem(String.to_integer(elem(x,1)), 100)
+						if elem(x,0) == "L" do
+							
+						newval = if remain>acc do (acc-remain)+100 else (acc-remain) end
+						{newval, newval} 
+						else 
+						if elem(x,0) == "C" do
+						{String.to_integer(elem(x,1)), String.to_integer(elem(x,1))}
+						else
+						newval = rem(acc+remain,100)
+						{newval,newval}
+						end
+						end
+					end)
+IO.inspect(abc)
+password = Enum.count(elem(abc,0), fn x -> x == 0 end)
+IO.inspect(password)
